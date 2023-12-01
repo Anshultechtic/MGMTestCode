73326506f16cc626bb4191d2d2c46f94788c6e09
Anshultechtic <139873271+Anshultechtic@users.noreply.github.com>
2023-12-01 18:55:40 +0530

    added DB queries package

---
+++ src/main/java/dbQueries/DBQueries.java
@@ -0,0 +1,5 @@
+package dbQueries;
+
+public class DBQueries {
+
+}
--- src/test/java/sqlTests/DBTestCases.java
+++ src/test/java/sqlTests/DBTestCases.java
@@ -21,6 +21,7 @@ public class DBTestCases extends BaseClass {
 		Statement stat = connection.createStatement();
 		String rowIDTG = null;
 		String rowOfRowID = null;
+		String rowOfRowColName = null;
 		int dataTypeRowID = 0;
 		String columnName = null;
 		ResultSet r = stat.executeQuery(
@@ -30,19 +31,20 @@ public class DBTestCases extends BaseClass {
 				+ "	\"t-Row\".\"Share\", \"t-Data\".\"Data-Type\",  \"t-Data\".\"Row-Data\", \"t-Data\".\"Bool-Data\", \r\n"
 				+ "	\"t-Data\".\"BigInt-Data\", \r\n"
 				+ "	\"t-Data\".\"JSON-Data\"->> '20000174' as mlText\r\n"
-				+ "	FROM \"AIC\".\"t-Item\"\r\n"
+				+ "	FROM \"public\".\"t-Item\"\r\n"
 				+ "	join \"t-Cell\" on \"t-Cell\".\"Cell\" = \"t-Item\".\"Cell\"\r\n"
 				+ "	join \"t-Col\" on \"t-Cell\".\"Col\" = \"t-Col\".\"Col\"\r\n"
 				+ "	join \"t-Row\" on \"t-Cell\".\"Row\"= \"t-Row\".\"Row\"\r\n"
-				+ "	join \"t-Data\" on \"t-Data\".\"Data\" = \"t-Item\".\"Data\" \r\n"
-				+ "	 where \"t-Item\".\"Item\" between 50002475 and 50002481;");
+				+ "	join \"t-Data\" on \"t-Data\".\"Data\" = \"t-Item\".\"Data\" \r\n");
+//				+ "	 where \"t-Item\".\"Item\" between 50002475 and 50002481;"
 
 		while (r.next()) {
 
 			rowIDTG = r.getString("Row");
 			 rowOfRowID = r.getString("Row");
-			System.out.println(rowIDTG + " "+  rowOfRowID);
-			ExcelCode.writeData(rowIDTG);
+			 rowOfRowColName = r.getString("ColName");
+			System.out.println(rowIDTG + " "+  rowOfRowID+" "+ rowOfRowColName);
+			
 
 		}
 	}
